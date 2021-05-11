c182176704b7d74dce923831eb058f3a410d08e0
Abhijeet Gaikwad <abhijeet.gaikwad058@gmail.com>
2021-05-11 20:15:00 +0530

    Update cowin-schedule.js

--- cowin-schedule.js
+++ cowin-schedule.js
@@ -2,6 +2,7 @@ var pincodes = [411027, 411026]
 var beneficiary = 67349625405990
 var captcha = "kkudR"
 var doseNumer = 1
+var token = ""
 
 data = await fetch("https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=363&date=11-05-2021").then(d => d.json())
 
@@ -17,7 +18,7 @@ if(validCenter.size > 0 ) {
 		  "headers": {
 		    "accept": "application/json, text/plain, /",
 		    "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-		    "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJiYWM4N2NmYS04MjI4LTRmMmMtODEzYi1kNWYzMDg3NWEwMDQiLCJ1c2VyX2lkIjoiYmFjODdjZmEtODIyOC00ZjJjLTgxM2ItZDVmMzA4NzVhMDA0IiwidXNlcl90eXBlIjoiQkVORUZJQ0lBUlkiLCJtb2JpbGVfbnVtYmVyIjo4ODg4NTgxNzEzLCJiZW5lZmljaWFyeV9yZWZlcmVuY2VfaWQiOjI3MTk5Nzk3ODA4NDQwLCJzZWNyZXRfa2V5IjoiYjVjYWIxNjctNzk3Ny00ZGYxLTgwMjctYTYzYWExNDRmMDRlIiwidWEiOiJNb3ppbGxhLzUuMCAoWDExOyBMaW51eCB4ODZfNjQpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS84NS4wLjQxODMuMTIxIFNhZmFyaS81MzcuMzYiLCJkYXRlX21vZGlmaWVkIjoiMjAyMS0wNS0xMVQxNDoyMTo1Ni4zOTBaIiwiaWF0IjoxNjIwNzQyOTE2LCJleHAiOjE2MjA3NDM4MTZ9.suSkyJAsJM8eAbwE-9HYymYmOy9xXW-ivGnzhowLQ1Q",
+		    "authorization": "Bearer " + token,
 		    "content-type": "application/json",
 		    "sec-fetch-dest": "empty",
 		    "sec-fetch-mode": "cors",
