e23c362380552351a29e242b497737702281cb67
Abhijeet Gaikwad <abhijeet.gaikwad058@gmail.com>
2021-05-11 20:16:29 +0530

    Update cowin-schedule.js

--- cowin-schedule.js
+++ cowin-schedule.js
@@ -3,8 +3,10 @@ var beneficiary = 67349625405990
 var captcha = "kkudR"
 var doseNumer = 1
 var token = ""
+var districtId = 363
+var date = "11-05-2021"
 
-data = await fetch("https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=363&date=11-05-2021").then(d => d.json())
+data = await fetch("https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=" + districtId + "&date=" + date).then(d => d.json())
 
 var validCenters = data.centers.filter(c => pincodes.includes(c.pincode) in c.sessions[0].min_age_limit <45 && c.sessions[0].available_capacity > 0)
 
